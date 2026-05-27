9f5c3a6cff9be6b1d1426fbac4e1cb8d3541e082
emirontop1 <dincemr@gmail.com>
2026-05-27 17:55:10 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,76 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
+#include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
 
-class EmirMenu : public FLAlertLayer { // geode::Popup yerine doğrudan FLAlertLayer miras al
+// 1. Popup Sınıfı: Referans dosyasındaki FLAlertLayer ve Popup mantığına uygun
+class EmirMenu : public geode::Popup<> {
+protected:
+    // setup() fonksiyonu geode::Popup'ın standartıdır
+    bool setup() override {
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        
+        // Başlık
+        auto title = CCLabelBMFont::create("Emir Mod", "bigFont.fnt");
+        title->setPosition({winSize.width / 2, winSize.height / 2 + 60});
+        this->m_mainLayer->addChild(title);
+
+        // Menu oluştur (Toggle'lar için taşıyıcı)
+        auto menu = CCMenu::create();
+        this->m_mainLayer->addChild(menu);
+
+        // Noclip Toggle
+        auto noclip = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(EmirMenu::onToggle), 0.8f);
+        noclip->setPosition({0, 20});
+        menu->addChild(noclip);
+
+        // ESP Toggle
+        auto esp = CCMenuItemToggler::createWithStandardSprites(this, menu_selector(EmirMenu::onToggle), 0.8f);
+        esp->setPosition({0, -20});
+        menu->addChild(esp);
+
+        return true;
+    }
+
+    void onToggle(CCObject* sender) {
+        Notification::create("Ayar Değiştirildi!", NotificationIcon::Success)->show();
+    }
+
 public:
     static EmirMenu* create() {
         auto ret = new EmirMenu();
-        if (ret && ret->init(nullptr, "Emir Mod", "Tamam", nullptr, 300)) {
+        // init(width, height)
+        if (ret && ret->init(300, 200)) {
             ret->autorelease();
             return ret;
         }
         CC_SAFE_DELETE(ret);
         return nullptr;
     }
-
-    bool init(const char* title, const char* content, const char* btn1, const char* btn2, float width) {
-        if (!FLAlertLayer::init(nullptr, title, content, btn1, btn2, width)) return false;
-        
-        // Buraya Toggle butonlarını ekleyebilirsin
-        return true;
-    }
 };
 
+// 2. Ana Menü Modifikasyonu
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
-        // Butonun buraya gelecek...
+
+        auto menu = static_cast<CCMenu*>(this->getChildByID("bottom-menu"));
+        
+        auto btn = CCMenuItemSpriteExtra::create(
+            ButtonSprite::create("Emir Mod"), 
+            this, 
+            menu_selector(MyMenuLayer::onOpenEmir)
+        );
+        
+        btn->setID("emir-btn"_spr);
+        menu->addChild(btn);
+        menu->updateLayout();
+
         return true;
+    }
+
+    void onOpenEmir(CCObject* sender) {
+        EmirMenu::create()->show();
     }
 };
