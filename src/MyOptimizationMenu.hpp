6643692b67bd11d53d51b800293120c5e69ebed9
Fabygamer1277 <gatospros24@gmail.com>
2026-07-09 12:16:42 -0600

    Remove MyOptimizationMenu class definition

--- src/MyOptimizationMenu.hpp
+++ src/MyOptimizationMenu.hpp
@@ -1,14 +1,1 @@
-#pragma once
-#include <Geode/Geode.hpp>
-
-using namespace geode::prelude;
-
-class MyOptimizationMenu : public geode::Popup<std::string const&> {
-protected:
-    // El método setup ahora recibe el string configurado en el template de la clase base
-    bool setup(std::string const& title) override;
-
-public:
-    // El método create ahora acepta el string que le pasaremos desde el Hook
-    static MyOptimizationMenu* create(std::string const& title);
-};
+gemini me paso un codigo pero que debo hacer con el?
