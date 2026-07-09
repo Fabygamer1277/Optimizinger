8c8cc4526c83070232c991c4ed29693207b61308
Fabygamer1277 <gatospros24@gmail.com>
2026-07-08 23:16:47 -0600

    Refactor MyOptimizationMenu class
    Refactor MyOptimizationMenu to use a template-less Popup and update setup method.

--- src/MyOptimizationMenu.hpp
+++ src/MyOptimizationMenu.hpp
@@ -2,9 +2,10 @@
 #include <Geode/Geode.hpp>
 #include <Geode/ui/Popup.hpp>
 
-class MyOptimizationMenu : public geode::Popup<std::string const&> {
+class MyOptimizationMenu : public geode::Popup<> {
 protected:
-    bool setup(std::string const& value) override;
+    bool setup() override; // Geode espera setup() sin argumentos si la plantilla es vacía
+    std::string m_value;
 
 public:
     static MyOptimizationMenu* create(std::string const& value);
