277c06665669b333f222738f445224a2dde7f02c
Fabygamer1277 <gatospros24@gmail.com>
2026-07-09 00:17:34 -0600

    Fix missing Popup include in MyOptimizationMenu
    Added missing Popup include for Geode 5.x compatibility.

--- src/MyOptimizationMenu.hpp
+++ src/MyOptimizationMenu.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <Geode/Geode.hpp>
-#include <Geode/ui/Popup.hpp> // <-- ¡ESTA LINEA FALTA Y CORRIGE LOS 20 ERRORES!
+#include <Geode/ui/Popup.hpp> // <-- ESENCIAL PARA GEODE 5.x
 
 class MyOptimizationMenu : public geode::Popup<std::string const&> {
 protected:
     bool setup(std::string const& value) override;
+
 public:
     static MyOptimizationMenu* create(std::string const& value);
 };
