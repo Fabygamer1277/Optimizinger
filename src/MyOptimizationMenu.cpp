40e58ef22e6d1c04dfc62e75640cb7fe3b760bbd
Fabygamer1277 <gatospros24@gmail.com>
2026-07-08 23:17:42 -0600

    Refactor MyOptimizationMenu for improved clarity

--- src/MyOptimizationMenu.cpp
+++ src/MyOptimizationMenu.cpp
@@ -1,12 +1,14 @@
 #include "MyOptimizationMenu.hpp"
-#include <cocos-ext.h> // <- Necesario para CCScale9Sprite
+#include <cocos-ext.h>
 
 using namespace cocos2d;
-using namespace cocos2d::extension; // <- Espacio de nombres para los sprites de escala 9
+using namespace cocos2d::extension;
 
 MyOptimizationMenu* MyOptimizationMenu::create(std::string const& value) {
     auto ret = new MyOptimizationMenu();
-    if (ret && ret->initAnchored(360.f, 240.f, value)) {
+    ret->m_value = value; // Guardamos el valor antes de inicializar si se ocupa
+    
+    if (ret && ret->initAnchored(360.f, 240.f)) {
         ret->autorelease();
         return ret;
     }
@@ -14,48 +16,42 @@ MyOptimizationMenu* MyOptimizationMenu::create(std::string const& value) {
     return nullptr;
 }
 
-bool MyOptimizationMenu::setup(std::string const& value) {
-    // Definimos el título nativo
+bool MyOptimizationMenu::setup() {
     this->setTitle("FPS OPTIMIZER");
 
-    // Ocultar el fondo marrón original
-    if (this->m_bgSprite) {
-        this->m_bgSprite->setVisible(false);
+    if (m_bgSprite) {
+        m_bgSprite->setVisible(false);
     }
 
-    // Cargar tu fondo personalizado morado usando el helper de Geode
     auto bgMorado = CCScale9Sprite::create("menu_purple.png"_spr);
     if (bgMorado) {
-        bgMorado->setContentSize(this->m_size);
-        bgMorado->setPosition(this->m_size / 2);
-        this->m_mainLayer->addChild(bgMorado, -1);
+        bgMorado->setContentSize(m_size);
+        bgMorado->setPosition(m_size / 2);
+        m_mainLayer->addChild(bgMorado, -1);
     }
 
-    // Reposicionar de manera segura el botón de cerrar nativo de Geode
-    if (this->m_closeBtn) {
-        this->m_closeBtn->setPosition({ -this->m_size.width / 2 + 15.f, this->m_size.height / 2 - 15.f });
+    if (m_closeBtn) {
+        m_closeBtn->setPosition({ -m_size.width / 2 + 15.f, m_size.height / 2 - 15.f });
     }
 
-    // Interfaz interna: Etiquetas de texto
+    // Interfaz del menú
     auto tpsLabel = CCLabelBMFont::create("Ticks per Second (TPS):", "bigFont.fnt");
     tpsLabel->setScale(0.4f);
-    tpsLabel->setPosition({ this->m_size.width / 2, this->m_size.height / 2 + 30.f });
-    this->m_mainLayer->addChild(tpsLabel);
+    tpsLabel->setPosition({ m_size.width / 2, m_size.height / 2 + 30.f });
+    m_mainLayer->addChild(tpsLabel);
 
-    // Obtener valor actual guardado en Geode
     int currentTPS = geode::Mod::get()->getSavedValue<int>("tps", 60);
     std::string tpsStr = std::to_string(currentTPS);
     
     auto tpsValue = CCLabelBMFont::create(tpsStr.c_str(), "bigFont.fnt");
     tpsValue->setScale(0.5f);
     tpsValue->setColor({ 0, 255, 0 });
-    tpsValue->setPosition({ this->m_size.width / 2, this->m_size.height / 2 });
-    this->m_mainLayer->addChild(tpsValue);
+    tpsValue->setPosition({ m_size.width / 2, m_size.height / 2 });
+    m_mainLayer->addChild(tpsValue);
 
     return true;
 }
 
 void MyOptimizationMenu::onClose(CCObject* sender) {
-    // Delegamos el cierre seguro a la clase base de Geode para evitar fugas de memoria
-    geode::Popup<std::string const&>::onClose(sender);
+    geode::Popup<>::onClose(sender);
 }
